151f8eb4c90f870f875c14913e386e0677006ddb
LuqmanAbdulrahman <luqmanar777@gmail.com>
2023-05-12 02:53:47 +0700

    .

--- project/app/entity/costumer.go
+++
@@ -1,8 +0,0 @@
-package entity
-
-type Customer struct {
-	ID          uint   `json:"id" gorm:"primaryKey"`
-	Name        string `json:"name"`
-	Address     string `json:"address"`
-	PhoneNumber string `json:"phone_number"`
-}
